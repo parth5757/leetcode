b3403e0b7af14b88a15eb9d74012081288805680
Parth Thakkar <psthakkar2@gmail.com>
2025-04-30 00:05:00 +0530

    count number of subarray have given minK and maxK using the linear approach(without even the checking any subarray).

--- python/2444.py
+++ python/2444.py
@@ -10,8 +10,12 @@ Return the number of fixed-bound subarrays.
 
 A subarray is a contiguous part of an array.
 '''
+import time
+import tracemalloc
 # method 1
+print("method 1")
 # brute force approach (too much time and memory(space) consuming and not suitable for the large dataset.)
+# time and space complexity of this code is O(n)
 class Solution(object):
     def countSubarrays(self, nums, minK, maxK):
         def min_max(arr, minV, maxV):
@@ -40,6 +44,48 @@ class Solution(object):
         # print(count)
         return count
 # arr = [1,2,3,4,5]
+# arr = [1,3,5,2,4,5]
 arr = [1,3,5,2,7,5]
+
 sol = Solution()
-print(sol.countSubarrays(arr, 1, 5))
+
+print(sol.countSubarrays(arr, 2, 5))
+
+
+# method 2
+class Solution(object):
+    def countSubarrays(self, nums, minK, maxK):
+        start = time.time()
+        tracemalloc.start()        
+        count = 0
+        minpos = maxpos = -1
+        left_bound = -1
+        for i, num in enumerate(nums):
+            # print(i, num)
+            if num < minK or num > maxK:
+                left_bound = i
+                # print(left_bound, num)
+            if num == minK:
+                minpos = i
+            if num == maxK:
+                maxpos = i
+            # print(minpos, maxpos, left_bound)
+            # print("2: ", count, max(0, min(minpos, maxpos)-left_bound), min(minpos, maxpos)-left_bound)
+            count += max(0, min(minpos, maxpos)-left_bound)
+
+        # Calculate space complexity
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        print(f"- Current memory usage: {current / 1024:.2f} KB")
+        print(f"- Peak memory usage: {peak / 1024:.2f} KB")
+        print(time.time()-start)
+        return count
+    
+# arr = [1,3,5,2,4,5]
+arr = [1,3,5,2,7,5]
+# arr = [1,2,3,4,5]
+
+sol = Solution()
+print("method 2")
+
+print(sol.countSubarrays(arr, 2, 5))
